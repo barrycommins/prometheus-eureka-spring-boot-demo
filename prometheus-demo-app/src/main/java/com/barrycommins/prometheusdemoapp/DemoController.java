d5ddd20706a6171ee7d76cefbd061f9e39d556e7
Barry Commins <barry.commins@gmail.com>
2023-04-05 22:32:57 +0100

    upgrade to Spring boot 3 and Java 17

--- prometheus-demo-app/src/main/java/com/barrycommins/prometheusdemoapp/DemoController.java
+++ prometheus-demo-app/src/main/java/com/barrycommins/prometheusdemoapp/DemoController.java
@@ -1,9 +1,11 @@
 package com.barrycommins.prometheusdemoapp;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/")
+@RestController
+@RequestMapping("/")
 class DemoController {
 
     @GetMapping
